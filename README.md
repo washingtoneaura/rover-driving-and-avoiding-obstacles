7b5957683a4c761f2d028ccb8c783f82b43e1d88
Washingtone Aura <aurawashingtone@gmail.com>
2024-11-04 19:07:02 +0300

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://washingtoneaura.github.io/rover-driving-and-avoiding-obstacles/](https://washingtoneaura.github.io/rover-driving-and-avoiding-obstacles/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/washingtoneaura/rover-driving-and-avoiding-obstacles** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/washingtoneaura/rover-driving-and-avoiding-obstacles** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,57 @@
+input.onButtonPressed(Button.A, function () {
+    isMoving = 1
+    hummingbird.setTriLED(
+    TwoPort.One,
+    0,
+    100,
+    0
+    )
+    hummingbird.setRotationServo(FourPort.One, 50)
+    hummingbird.setRotationServo(FourPort.Two, -50)
+})
+input.onButtonPressed(Button.B, function () {
+    isMoving = 0
+    hummingbird.setTriLED(
+    TwoPort.One,
+    100,
+    0,
+    0
+    )
+    hummingbird.setRotationServo(FourPort.One, 0)
+    hummingbird.setRotationServo(FourPort.Two, 0)
+})
+let isMoving = 0
+hummingbird.startHummingbird()
+basic.forever(function () {
+    if (isMoving == 1) {
+        if (hummingbird.getSensor(SensorType.Distance, ThreePort.One) < 15) {
+            hummingbird.setTriLED(
+            TwoPort.One,
+            100,
+            0,
+            0
+            )
+            hummingbird.setRotationServo(FourPort.One, -50)
+            hummingbird.setRotationServo(FourPort.Two, 50)
+            basic.pause(1000)
+            hummingbird.setRotationServo(FourPort.One, 0)
+            hummingbird.setRotationServo(FourPort.Two, 0)
+            isMoving = 0
+        }
+    } else {
+        if (hummingbird.getSensor(SensorType.Distance, ThreePort.One) >= 15) {
+            hummingbird.setTriLED(
+            TwoPort.One,
+            0,
+            100,
+            0
+            )
+            hummingbird.setRotationServo(FourPort.One, 50)
+            hummingbird.setRotationServo(FourPort.Two, -50)
+            isMoving = 1
+        } else {
+            hummingbird.setRotationServo(FourPort.One, 0)
+            hummingbird.setRotationServo(FourPort.Two, 0)
+        }
+    }
+})
